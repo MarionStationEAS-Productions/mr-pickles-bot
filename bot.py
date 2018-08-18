61cf32b0cf5a5bdca47d568ed6d59b48970a6770
MazariFox~ <mazarifox@gmail.com>
2018-08-18 16:44:24 -0500

    Add info/edits to main

--- bot.py
+++ bot.py
@@ -9,12 +9,17 @@ client = commands.Bot(command_prefix = "p!")
 
 @client.event
 async def on_ready():
-    print("Bot is ready!")
+    print("*I have been summoned...*")
 
 @client.event
 async def on_message(message):
     if message.content == "ping":
-        await  client.send_message(message.channel, "<a:twctstorm:460501169375281172>")
+        await  client.send_message(message.channel, "<a:twctstorm:460501169375281172> Coming Shortly")
+		
+@client.event
+async def on_message(message):
+    if message.content == "info":
+        await  client.send_message(message.channel, "Mr. Pickles | Ver. 0.0.1 | Developed by MZFX18#0069 & JoshTheGamer632#0017")
 
 
 
